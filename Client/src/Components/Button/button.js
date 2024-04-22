c0bf7b692faf8ae21a99a70a674739fd87465962
jamesjoy-2k24 <jamesjoy2k24@gmail.com>
2024-04-22 10:54:15 +0530

    Created React for my project

--- Client/src/Components/Button/button.js
+++
@@ -1,9 +0,0 @@
-import "./button.css";
-
-export default function Button(props) {
-    return (
-        <div className="button">
-            <button onClick={props.onClick} type={props.type}>{props.text}</button>
-        </div>
-    );
-}
